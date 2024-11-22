b4325b543ce4a56546d360c508321f451a8a4687
adriano-551 <32731609+adriano-551@users.noreply.github.com>
2024-11-22 12:35:32 +0000

    Added weather model

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+
+class Weather():
+    country_id: int
+    weather_type: str
+    temp: float
+    pressure: int
+    humidity: int
+    temp_min: float
+    temp_max: float
+    country: str
+    name: str
